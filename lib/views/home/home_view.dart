06ece78e5228e7dbd2296446b5a45e4c9609c603
Sean London <slondon@siue.edu>
2021-03-03 17:26:37 -0600

    Began adding navigation methods. Need to add app bar to township menu.

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:madcotowers_1/townshipMenu.dart';
+import 'package:madcotowers_1/widgets/navigation_bar/centered_view/centered_view.dart';
+import 'package:madcotowers_1/widgets/navigation_bar/course_details/course_details.dart';
 import 'package:madcotowers_1/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,8 +10,24 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var pageNavigatedTo = await Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (context) =>
+                      townshipMenu())); //navigates to township menu page, awaits result, then stores the destination
+        },
+      ),
       backgroundColor: Colors.white,
-      body: Column(children: <Widget>[NavigationBar()]),
+      body: CenteredView(
+          child: Column(children: <Widget>[
+        NavigationBar(),
+        Expanded(
+            child: Row(
+          children: <Widget>[CourseDetails()],
+        ))
+      ])),
     );
   }
 }
