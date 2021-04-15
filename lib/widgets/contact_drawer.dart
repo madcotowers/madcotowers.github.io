4097ae990c5636947f444ae826d97c1d3dbc23a3
SeanLondon <33425528+SeanLondon@users.noreply.github.com>
2021-04-15 14:46:10 -0500

    Added hidden drawer functionality

--- lib/widgets/contact_drawer.dart
+++ lib/widgets/contact_drawer.dart
@@ -1,40 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:madcotowers_1/CustomMaterialColor.dart';
+
 
 class ContactDrawer extends StatelessWidget {
-  const ContactDrawer({Key key}) : super(key: key);
+  ContactDrawer({Key key}) : super(key: key);
+  final customColor = CustomMaterialColor().createMaterialColor(Color(0xFF8b0d04));
+  
+  
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-
-    return Container(
-      width: width,
-      height: height / 3,
-      child: Column(
-        children: <Widget>[
-          Icon(
-            Icons.keyboard_arrow_up,
-            size: 20,
-          ),
-          Text(
-            "Contact Us",
-            style: TextStyle(
-                fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          Text(
-            "Email: **************",
-            style: TextStyle(fontSize: 10, color: Colors.white),
-          ),
-          Text(
-            "Phone: ***-***-****",
-            style: TextStyle(fontSize: 10, color: Colors.white),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      child: Container(
+        color: customColor,
+        width: width,
+        height: height / 3,
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.keyboard_arrow_up,
+              size: 20,
+            ),
+            Text(
+              "Contact Us",
+              style: TextStyle(
+                  fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            Text(
+              "Email: **************",
+              style: TextStyle(fontSize: 10, color: Colors.white),
+            ),
+            Text(
+              "Phone: ***-***-****",
+              style: TextStyle(fontSize: 10, color: Colors.white),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      )
     );
+    }
   }
 }
